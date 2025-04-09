2486826149f5543e1d6a331e5ace8055e2eba980
Ольга Чулакова <olka.13@list.ru>
2025-04-10 01:38:35 +0300

    добавляет спрайт и стилизацию шапки

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,10 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+const buttonMenu = document.querySelector('.menu__button');
+const menu = document.querySelector('.menu__nav');
+
+buttonMenu.onclick = () => {
+  menu.classList.toggle('active');
+};
