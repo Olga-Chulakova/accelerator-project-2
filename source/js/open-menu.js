615c993d2057cc5431c87438051e64ddd526ceb0
Ольга Чулакова <olka.13@list.ru>
2025-04-30 02:53:39 +0300

    добавляет картинку на форму

--- source/js/open-menu.js
+++ source/js/open-menu.js
@@ -1,12 +1,27 @@
 const navMain = document.querySelector('.main-nav');
 const button = document.querySelector('.header__button');
+const menuList = document.querySelector('.menu__list');
+const menuLinks = menuList.querySelectorAll('.menu__link');
+const body = document.querySelector('.page-body');
 
 button.addEventListener('click', () => {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
+    body.classList.add('no-scroll');
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+    body.classList.remove('no-scroll');
   }
+});
+
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('click', () => {
+    if (menuLink && navMain.classList.contains('main-nav--opened')) {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+      body.classList.remove('no-scroll');
+    }
+  });
 });
