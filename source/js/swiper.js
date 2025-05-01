82dbb1ae3d238300b7bea64494bfbd05ed3280e2
Olga Chulakova <olka.13@list.ru>
2025-05-01 21:01:41 +0300

    правит стили к блоку о нас по PP

--- source/js/swiper.js
+++ source/js/swiper.js
@@ -93,35 +93,35 @@ import 'swiper/css/pagination';
 //   touchRatio: 1,
 // });
 
-const trainingSwiper = new Swiper ('.training-card__slider-wrapper', {
-  modules: [Navigation, Mousewheel],
-  allowTouchMove: true,
-  slidesPerView: 1,
-  slidesPerGroup: 1,
-  spaceBetween: 20,
-  initialSlide: 0,
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      initialSlide: 2,
-    },
-    768: {
-      slidesPerView: 3,
-      initialSlide: 0,
-    },
-    1440: {
-      slidesPerView: 4,
-    }
-  },
-  navigation: {
-    nextEl: '.training-card__slider-button--next',
-    prevEl: '.training-card__slider-button--prev',
-  },
-  simulateTouch: true,
-  touchRatio: 1,
-});
+// const trainingSwiper = new Swiper ('.training-card__slider-wrapper', {
+//   modules: [Navigation, Mousewheel],
+//   allowTouchMove: true,
+//   slidesPerView: 1,
+//   slidesPerGroup: 1,
+//   spaceBetween: 20,
+//   initialSlide: 0,
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1,
+//       initialSlide: 2,
+//     },
+//     768: {
+//       slidesPerView: 3,
+//       initialSlide: 0,
+//     },
+//     1440: {
+//       slidesPerView: 4,
+//     }
+//   },
+//   navigation: {
+//     nextEl: '.training-card__slider-button--next',
+//     prevEl: '.training-card__slider-button--prev',
+//   },
+//   simulateTouch: true,
+//   touchRatio: 1,
+// });
 
-trainingSwiper.init();
+// trainingSwiper.init();
 
 // const reviewsSwiper = new Swiper ('.reviews-card', {
 //   modules: [Navigation, Mousewheel],
